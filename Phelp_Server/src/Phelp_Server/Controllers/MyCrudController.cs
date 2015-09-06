bd6494ee0e5eefe08bcbc625acd1a3a1cf0bee95
zweii <marcelo_silva.313@hotmail.com>
2015-09-06 17:46:20 +0100

    classes adicionadas para crudcontroller (api) e gcm notification

---
+++ Phelp_Server/src/Phelp_Server/Controllers/MyCrudController.cs
@@ -0,0 +1,70 @@
+﻿using FindMyBeaconReal.Logic;
+using FindMyBeaconReal.Models;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Phelp_Server.Controllers
+{
+    public abstract class MyCrudController<T> : Controller
+    {
+        public abstract Task<IEnumerable<T>> GetAllCall();
+        public abstract Task<T> GetByIdCall(int id);
+        public abstract Task<T> CreateCall(T obj);
+        public abstract Task<T> UpdateCall(int id, T obj);
+        public abstract Task<IActionResult> DeleteCall(int id);
+
+        [HttpGet]
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            var result = await GetAllCall();
+            return result;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var obj = await GetByIdCall(id);
+            if (obj == null) return HttpNotFound();
+            return new ObjectResult(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] T obj)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(obj);
+            }
+            else
+            {
+                obj = await CreateCall(obj);
+                return new ObjectResult(obj);
+            }
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] T obj)
+        {
+            if (!ModelState.IsValid)
+            {
+                return HttpBadRequest(obj);
+            }
+            else
+            {
+                var exists = await UpdateCall(id, obj);
+                if (exists == null) return HttpNotFound();
+                return new HttpStatusCodeResult(204);
+
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return await DeleteCall(id);
+        }
+    }
+}
---
+++ Phelp_Server/src/Phelp_Server/Services/AndroidGCMPushNotification.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phelp_Server.Services
+{
+    public class AndroidGCMPushNotification
+    {
+        private const string SERVER = "https://gcm-http.googleapis.com/gcm/send";
+
+        //AQUI SERÀ O NOSSO ID GOOGLE APP E SENDER
+        private const string GOOGLE_APP_ID = "AIzaSyDJUvYm_3TSpViNey1sYvg4A9E1DNl47BU";
+        private const string SENDER_ID = "218667633387";
+
+        public async Task<string> Send<T>(T postObject)
+        {
+            HttpResponseMessage result = null;
+            using (var http = new HttpClient())
+            {
+                Uri theUri = new Uri(SERVER);
+                http.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", "key=" + GOOGLE_APP_ID);
+                http.DefaultRequestHeaders.TryAddWithoutValidation("Sender", "id=" + SENDER_ID);
+                http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                http.DefaultRequestHeaders.Host = theUri.Host;
+                try
+                {
+                    result = await http.PostAsync(theUri, postObject, new JsonMediaTypeFormatter());
+                }
+                catch (Exception e)
+                {
+                    string s = e.StackTrace;
+                }
+            }
+
+            return result.Content.ToString();
+        }
+    }
+}
